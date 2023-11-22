74469920ff0dd228d9b9ab8b80eb7438b38fe9fe
olel <olel@olel.de>
2023-11-22 13:53:15 +0100

    Added half-height columns

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -3,13 +3,18 @@ import "./column.css";
 
 interface SCSColumnParams {
   title: string;
+  halfHeight?: boolean;
   children?: any;
 }
 
 function SCSColumn(params: SCSColumnParams) {
   return (
     <>
-      <Card className="SCSColumn">
+      <Card
+        className={
+          params.halfHeight || false ? "SCSColumnHalfHeight" : "SCSColumn"
+        }
+      >
         <CardHeader className="SCSColHeader">
           <div className="fw-bold">{params.title}</div>
         </CardHeader>
