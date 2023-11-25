1611e199759598aab832ddd8f189e910fefdb601
olel <olel@olel.de>
2023-11-25 17:33:15 +0100

    Implemented got hit

--- src/components/visible/got_hit.tsx
+++ src/components/visible/got_hit.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Logger from "../../util/logger";
+import SocketManager from "../../util/socketManager";
 
 function GotHit() {
+  const [pid, setPid] = useState(0);
+  const [sid, setSid] = useState(0);
+  const [hp, setHp] = useState(0);
+
   return (
     <>
       <Form.Group>
@@ -9,17 +16,23 @@ function GotHit() {
           type="number"
           min="0"
           max="65535"
-          value={0}
+          value={pid}
+          onChange={(e) => setPid(parseInt(e.target.value))}
         ></Form.Control>
         <Form.Label>SID:</Form.Label>
         <Form.Control
           type="number"
           min="0"
           max="65535"
-          value={0}
+          value={sid}
+          onChange={(e) => setSid(parseInt(e.target.value))}
         ></Form.Control>
         <Form.Label>Hitpoint:</Form.Label>
-        <Form.Select>
+        <Form.Select
+          onChange={(e) => {
+            setHp(parseInt(e.target.value));
+          }}
+        >
           <option value={0}>Phaser</option>
           <option value={1}>Chest</option>
           <option value={2}>Back</option>
@@ -30,7 +43,19 @@ function GotHit() {
           <option value={7}>Undefined (7)</option>
         </Form.Select>
       </Form.Group>
-      <Button className="mt-2 w-100 btn-danger">Got Hit</Button>
+      <Button
+        className="mt-2 w-100 btn-danger"
+        onClick={() => {
+          SocketManager.Instance.send({
+            a: [6],
+            pid: pid,
+            sid: sid,
+            hp: hp,
+          });
+        }}
+      >
+        Got Hit
+      </Button>
     </>
   );
 }
