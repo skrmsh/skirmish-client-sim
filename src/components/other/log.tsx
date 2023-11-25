5a3bbba07e1493dd8368cba2d5cf427ee6c09306
olel <olel@olel.de>
2023-11-25 17:03:30 +0100

    Implemented input text box

--- src/components/other/log.tsx
+++ src/components/other/log.tsx
@@ -1,9 +1,13 @@
 import { useRef, useState } from "react";
 import Logger from "../../util/logger";
 import "./log.css";
+import { Button, Form } from "react-bootstrap";
+import SocketManager from "../../util/socketManager";
 
 function LogDisplay() {
   const [logText, setLogText] = useState("");
+
+  const [inputText, setInputText] = useState("");
 
   const bottomEl = useRef<null | HTMLDivElement>(null);
 
@@ -16,11 +20,36 @@ function LogDisplay() {
   });
 
   return (
-    <>
-      <div id="logContainer" className="log" ref={bottomEl}>
-        {logText}
+    <div className="logParent">
+      <div id="logContainer" className="log">
+        <span ref={bottomEl}>{logText}</span>
       </div>
-    </>
+      <hr />
+      <div className="logInput">
+        <Form.Group>
+          <Form.Label>Input (→ WS 'message' on return)</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="JSON Data"
+            onChange={(e) => {
+              setInputText(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                try {
+                  let data = JSON.parse(inputText);
+                  if (!!data) {
+                    SocketManager.Instance.send(data);
+                  }
+                } catch (error) {
+                  Logger.log("Invalid Input");
+                }
+              }
+            }}
+          />
+        </Form.Group>
+      </div>
+    </div>
   );
 }
 
